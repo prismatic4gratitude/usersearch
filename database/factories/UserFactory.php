ab8201c13b9be70b1380d4635e3b06cd263e4196
prismatic4gratitude <tomiwa.gratitudeindia@gmail.com>
2020-09-16 13:33:02 +0100

    feat: done with seeder and db functions

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,12 +22,14 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $gender_array = ['male','female'];
         return [
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'phone_number' => $this->faker->e164PhoneNumber,
+            'gender' => $gender_array[array_rand($gender_array)], // password
+            // 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            // 'remember_token' => Str::random(10),
         ];
     }
 }
